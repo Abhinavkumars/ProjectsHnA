60fdd1c8e5689e6bd2a22eaf1e2d116179bba3d7
Abhi_now <104564674+Abhinavkumars@users.noreply.github.com>
2024-03-03 20:08:06 +0530

    Fifth Commit

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -1,19 +1,43 @@
 import Tile from '../Tile/Tile';
 import './Chessboard.css';
+
 const HorizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const VerticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
 const Chessboard = () => {
-    
-
     let board = [];
+
+    const Pieces = [];
+
+    for (let p = 0; p < 2; p++) {
+        const type = p === 0 ? 'b' : 'w';
+        const y = p === 0 ? 7 : 0;
+        Pieces.push({ image: `/images/rook_${type}.png`, x: 0, y });
+        Pieces.push({ image: `images/bishop_${type}.png`, x: 2, y });
+        Pieces.push({ image: `images/knight_${type}.png`, x: 1, y });
+        Pieces.push({ image: `images/queen_${type}.png`, x: 3, y });
+        Pieces.push({ image: `images/king_${type}.png`, x: 4, y });
+        Pieces.push({ image: `/images/rook_${type}.png`, x: 7, y });
+        Pieces.push({ image: `images/bishop_${type}.png`, x: 5, y });
+        Pieces.push({ image: `images/knight_${type}.png`, x: 6, y });
+    }
+
+    for (let i = 0; i < 8; i++) {
+        Pieces.push({ image: "images/pawn_b.png", x: i, y: 6 });
+        Pieces.push({ image: "images/pawn_w.png", x: i, y: 1 });
+    }
 
     for (let j = VerticalAxis.length - 1; j >= 0; j--) {
         for (let i = 0; i < HorizontalAxis.length; i++) {
-            const number = i + j + 1;
-            let image = getImageForPosition(i, j); 
+            const number = i + j + 2;
+            let image = undefined;
+            Pieces.forEach((p) => {
+                if (p.x === i && p.y === j) {
+                    image = p.image;
+                }
+            });
             board.push(
-            <Tile key = {`${HorizontalAxis[i]}${VerticalAxis[j]}`} Number = {number} image = {image}/>
+                <Tile key={`${HorizontalAxis[i]}${VerticalAxis[j]}`} Number={number} image={image} />
             );
         }
     }
@@ -24,58 +48,5 @@ const Chessboard = () => {
         </div>
     );
 };
-function getImageForPosition(i, j) {
 
-    const Black_Pieces = {
-        'rook' : '/images/rook_b.png',
-        'bishop' : 'images/bishop_b.png',
-        'knight' : 'images/knight_b.png',
-        'queen' : 'images/queen_b.png',
-        'king' : 'images/king_b.png',
-        'pawn' : '/images/pawn_b.png',
-    }
-    const White_Pieces = {
-        'rook' : '/images/rook_w.png',
-        'bishop' : '/images/bishop_w.png',
-        'knight' : '/images/knight_w.png',
-        'queen' : '/images/queen_w.png',
-        'king' : '/images/king_w.png',
-        'pawn' : '/images/pawn_w.png',
-
-    }
-    if(j === 6){
-        return Black_Pieces['pawn']; // pawn 
-    }
-    if(j === 1){
-        return White_Pieces['pawn']; // pawn
-    }
-    if (j === 7) {
-        if (i === 0 || i === 7) {
-            return Black_Pieces['rook']; // Rook
-        } else if (i === 1 || i === 6) {
-            return Black_Pieces['knight']; // Knight
-        } else if (i === 2 || i === 5) {
-            return Black_Pieces['bishop']; // Bishop
-        } else if (i === 4) {
-            return Black_Pieces['queen']; // Queen
-        } else if (i === 3) {
-            return Black_Pieces['king']; // King
-        }
-    }
-    if (j === 0) {
-        if (i === 0 || i === 7) {
-            return White_Pieces['rook']; // Rook
-        } else if (i === 1 || i === 6) {
-            return White_Pieces['knight']; // Knight
-        } else if (i === 2 || i === 5) {
-            return White_Pieces['bishop']; // Bishop
-        } else if (i === 3) {
-            return White_Pieces['queen']; // Queen
-        } else if (i === 4) {
-            return White_Pieces['king']; // King
-        }
-    }
-    return undefined;
-}
 export default Chessboard;
-
