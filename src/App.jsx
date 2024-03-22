8591aa97477d2ed652f6d265380addf533812db3
Harshit <meenushukla987654321@gmail.com>
2024-03-22 18:58:07 +0530

    commit 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import './App.css';
+import Board from './components/Board/Board';
+import { reducer } from './reducer/reducer'
+import { useReducer } from 'react'
+import { initGameState } from './constants';
+import AppContext from './contexts/Context'
+import Control from './components/Control/Control';
+import TakeBack from './components/Control/bits/TakeBack';
+import MovesList from './components/Control/bits/MovesList';
 
-import './App.css'
-import Chessboard from "./components/Chessboard/Chessboard"
 function App() {
 
- 
-  return (
-    <>
-      <div className="app">
-        <Chessboard />
-      </div>
-      
-    </>
-  )
+    const [appState, dispatch ] = useReducer(reducer,initGameState);
+
+    const providerState = {
+        appState,
+        dispatch
+    }
+
+    return (
+        <AppContext.Provider value={providerState} >
+            <div className="App">
+                <Board/>
+                <Control>
+                    <MovesList/>
+                    <TakeBack/>
+                </Control>
+            </div>
+        </AppContext.Provider>
+    ); 
 }
 
-export default App
+export default App;
